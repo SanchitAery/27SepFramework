b9b693b803c774f0f80b4dc5d4f9a58b224f2cc0
Sanchit <sharmasanchit@seasiainfotech.com>
2019-11-27 09:47:57 +0530

    finished local framework

---
+++ 27SepFramework/27SepFramework/_7SepFramework.h
@@ -0,0 +1,19 @@
+//
+//  _7SepFramework.h
+//  27SepFramework
+//
+//  Created by Sanchit Sharma on 11/27/19.
+//  Copyright © 2019 Sanchit Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for _7SepFramework.
+FOUNDATION_EXPORT double _7SepFrameworkVersionNumber;
+
+//! Project version string for _7SepFramework.
+FOUNDATION_EXPORT const unsigned char _7SepFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <_7SepFramework/PublicHeader.h>
+
+
---
+++ 27SepFramework/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  27SepFramework
+//
+//  Created by Sanchit Sharma on 11/27/19.
+//  Copyright © 2019 Sanchit Sharma. All rights reserved.
+//
+
+import Foundation
+
+public class Service{
+    private init(){
+        
+    }
+    
+    public static func doSomething()-> String{
+        return "Did Some stuff, here you go"
+    }
+}
